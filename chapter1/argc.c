1742cb4d35f0d4321526ccda594a7c6addb54681
c1oud9 <69421277+c1oud9@users.noreply.github.com>
2025-01-10 15:53:07 +0900

    cc

--- chapter1/argc.c
+++ chapter1/argc.c
@@ -4,11 +4,10 @@
 int main(int argc, char *argv[])
 
 {
-	printf ("hello world");
+	printf ("hello world\n");
 }
 
 /*
 c언어 프로그램에서 main 함수는 2개의 매개변수를 가진다 
 명령행이란 프로그램을 실행할 때 프로그램 이름 뒤에 입력하는 문자열을 말한다.
-
 */
